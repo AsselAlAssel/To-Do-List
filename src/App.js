6ffb0ef72a11d2660e410553de46bce18d84af2e
AseelAlAseel <aseel1234500000@gmail.com>
2022-01-29 20:49:24 +0200

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 const App =()=>{
+    
     return(
         <div>
-            <p>
-                hello world
-            </p>
+           
         </div>
     )
 }
