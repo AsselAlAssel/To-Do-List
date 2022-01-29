472a945a7f32ad0ee599c7afff0e2addea324911
AseelAlAseel <aseel1234500000@gmail.com>
2022-01-29 21:19:31 +0200

    add state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import Head from './components/Head';
-const App =()=>{
-    
-    return(
-        <div>
-           <Head/>
-        </div>
-    )
-}
+import React, { useState } from "react";
+import Head from "./components/Head";
+const toDoItems = [
+  {
+    title: "read the react book",
+    stat: "available",
+  },
+  {
+    title: "read the css book",
+    stat: "completed",
+  },
+];
+const App = () => {
+  const [toDoList, setToDoList] = useState(toDoItems);
+
+  return (
+    <div>
+      <Head />
+    </div>
+  );
+};
 
 export default App;
